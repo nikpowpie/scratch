86686f5ca65700d7e4b5eb195947aa827f3abbfb
npowerpierre <nicolepowerpierre@gmail.com>
2016-04-04 18:46:09 -0400

    add repository files

---
+++ includes/widget-areas.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+* Register widget areas.
+*
+* @package    Scratch
+* @author     Nikki Pierre
+* @link       http://www.nikkipierre.com/
+* @copyright  Copyright (c) 2016, Nikki Pierre
+* @license    GPL-2.0+
+*/
+
+//* Register front page widget areas.
+genesis_register_sidebar( array(
+  'id'            => 'home-welcome',
+  'name'          => __( 'Home Welcome', 'scratch' ),
+  'description'   => __( 'This is a home widget area that will show on the front page', 'scratch' ),
+    ) );
+genesis_register_sidebar( array(
+  'id'            => 'call-to-action',
+  'name'          => __( 'Call to Action', 'scratch' ),
+  'description'   => __( 'This is a call to action widget area that will show on the front page', 'scratch' ),
+      ) );
